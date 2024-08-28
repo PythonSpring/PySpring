65c9d5179cf72e8cb9faf9dbd811dc7e1073676d
William Chen <wichen@sram.com>
2024-08-28 11:53:05 +0800

    add: Add public package imports for ComponentScope

--- py_spring/__init__.py
+++ py_spring/__init__.py
@@ -1,7 +1,7 @@
 from .core.entities.properties.properties import Properties
 from .persistence.repositories.crud_repository import CrudRepository
 from .core.entities.controllers.rest_controller import RestController
-from .core.entities.component import Component
+from .core.entities.component import Component, ComponentScope
 from .core.entities.bean_collection import BeanCollection
 from .persistence.core.py_spring_model import PySpringModel
 from .core.application.py_spring_application import PySpringApplication
