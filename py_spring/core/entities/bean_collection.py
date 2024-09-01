34feb52579d38bb35a1e41049ee22299dec5b069
William Chen <OW6201231@gmail.com>
2024-09-01 22:04:43 +0800

    refactor: Refactor BeanCollection to directly use the original bean creation function, removing the construct_bean_creation_func method and simplifying the bean initialization process.

--- py_spring/core/entities/bean_collection.py
+++ py_spring/core/entities/bean_collection.py
@@ -71,11 +71,10 @@ class BeanCollection:
 
             creation_func = getattr(cls, func_name)
             bean_name = creation_func.__annotations__[cls.RETURN_KEY].__name__
-            final_creation_func = cls.construct_bean_creation_func(creation_func)
             view = BeanView(
                 bean_name=bean_name,
-                bean_creation_func=final_creation_func,
-                bean=final_creation_func(),
+                bean_creation_func=creation_func,
+                bean=creation_func(),
             )
             logger.success(
                 f"[BEAN CREATION UNDER {cls.__name__}] Found bean creation func: {view.bean_creation_func.__name__} with bean name: {view.bean_name}"
@@ -83,24 +82,7 @@ class BeanCollection:
             bean_views.append(view)
 
         return bean_views
-
-    @classmethod
-    def construct_bean_creation_func(
-        cls, bean_creation_func: Callable[..., Component]
-    ) -> Callable[..., Component]:
-        properties_container = {
-            properties.get_name(): properties
-            for properties in _PropertiesLoader.optional_loaded_properties.values()
-        }
-        _kwargs = {}
-        for param_key, param_cls in bean_creation_func.__annotations__.items():
-            if param_key == cls.RETURN_KEY:
-                continue
-            properties_instance = properties_container.get(param_cls.__name__)
-            _kwargs[param_key] = properties_instance
-
-        return lambda: bean_creation_func(**_kwargs)
-
+    
     @classmethod
     def get_name(cls) -> str:
         return cls.__name__
