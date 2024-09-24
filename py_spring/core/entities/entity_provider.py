9d0a1700151cb9cd7dc4f27bf734b83601042ceb
William Chen <william_w_chen@trendmicro.com>
2024-09-24 22:23:24 +0800

    feat: extend EntityProvider to support RestController classes
    - Added `rest_controller_classes` attribute to `EntityProvider` to handle `RestController` entities.
    - Updated `get_entities` method to return `RestController` classes along with other entity types.

--- py_spring/core/entities/entity_provider.py
+++ py_spring/core/entities/entity_provider.py
@@ -4,6 +4,7 @@ from typing import Type
 
 from py_spring.core.entities.bean_collection import BeanCollection
 from py_spring.core.entities.component import Component
+from py_spring.core.entities.controllers.rest_controller import RestController
 from py_spring.core.entities.properties.properties import Properties
 from py_spring.persistence.core.py_spring_model import PySpringModel
 
@@ -13,13 +14,15 @@ class EntityProvider:
     component_classes: list[Type[Component]] = field(default_factory= list)
     bean_collection_classes: list[Type[BeanCollection]] = field(default_factory= list)
     properties_classes: list[Type[Properties]] = field(default_factory= list)
+    rest_controller_classes: list[Type[RestController]] = field(default_factory= list)
     model_classes: list[Type[PySpringModel]] = field(default_factory= list)
 
 
     def get_entities(self) -> list[Type[object]]:
         return [*self.component_classes, 
                 *self.bean_collection_classes, 
-                *self.properties_classes
+                *self.properties_classes,
+                *self.rest_controller_classes
                 ]
     
     def get_models(self) -> list[Type[PySpringModel]]:
