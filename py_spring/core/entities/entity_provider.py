9b56eefcfa33597fdb61f0320a39797cd9c5f3e4
William Chen <william_w_chen@trendmicro.com>
2024-10-01 01:33:00 +0800

    fix: Workaround circular dependency in `EntityProvider` for IDE support
    - Added a try-except block for `ApplicationContext` import to avoid circular dependency issues.
    - Used forward references for `app_context` and `set_context` type annotations to improve IDE compatibility.

--- py_spring/core/entities/entity_provider.py
+++ py_spring/core/entities/entity_provider.py
@@ -3,11 +3,16 @@ from dataclasses import field, dataclass
 from typing import Optional, Type
 
 from py_spring.core.application.commons import AppEntities
-from py_spring.core.application.context.application_context import ApplicationContext
 from py_spring.core.entities.bean_collection import BeanCollection
 from py_spring.core.entities.component import Component
 from py_spring.core.entities.controllers.rest_controller import RestController
 from py_spring.core.entities.properties.properties import Properties
+
+try:
+    from py_spring.core.application.context.application_context import ApplicationContext
+except ImportError:
+    ...
+
 
 @dataclass
 class EntityProvider:
@@ -16,7 +21,7 @@ class EntityProvider:
     properties_classes: list[Type[Properties]] = field(default_factory= list)
     rest_controller_classes: list[Type[RestController]] = field(default_factory= list)
     depends_on: list[Type[AppEntities]] = field(default_factory= list)
-    app_context: Optional[ApplicationContext] = None
+    app_context: Optional["ApplicationContext"] = None
 
     def get_entities(self) -> list[Type[object]]:
         return [*self.component_classes, 
@@ -25,7 +30,7 @@ class EntityProvider:
                 *self.rest_controller_classes
                 ]
     
-    def set_context(self, app_context: ApplicationContext) -> None:
+    def set_context(self, app_context: "ApplicationContext") -> None:
         self.app_context = app_context
 
     def provider_init(self) -> None: ...
