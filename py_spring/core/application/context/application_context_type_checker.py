9b7472962155f2407e675cf03eccc7d7d6ce5e60
William Chen <william_w_chen@trendmicro.com>
2024-10-02 22:30:53 +0800

    fix: type hints checker should raise error if member functions are not fully type hinted

--- py_spring/core/application/context/application_context_type_checker.py
+++ py_spring/core/application/context/application_context_type_checker.py
@@ -35,5 +35,6 @@ class ApplicationContextTypeChecker:
                         check_type_hints_for_class(_cls, skip_attrs=self.skip_class_attrs)
                     except TypeHintError as error:
                         logger.warning(f"Type hint error for class {_cls.__name__}: {error}")
+                        raise error
                     except NameError as error:
                         ...
